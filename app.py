d4adc5fcf79dbcbc6d375ee5f9d2407f6732a178
Htet Arkar Linn <62011557+htetarkarlinn@users.noreply.github.com>
2026-05-31 23:49:54 +0700

    Add Explainable AI and fix UI

--- app.py
+++ app.py
@@ -345,4 +345,4 @@ def compare():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
